101892003a55f18457a693ebc2c224fd3e485a67
Angel <angel.pashov@proton.me>
2025-01-29 23:54:22 +0200

    refactored the news page

--- app/news/page.js
+++ app/news/page.js
@@ -1,20 +1,13 @@
 import React from "react";
-import Link from "next/link";
 
 import { DUMMY_NEWS } from "@/dummy-news";
+import NewsList from "@/components/news-list";
 
 const NewsPage = () => {
     return (
         <>
             <h1>News Page</h1>
-            <ul className="news-list"> 
-                {DUMMY_NEWS.map((article) => (<li key={article.id}>
-                    <Link href={`/news/${article.slug}`}>
-                        <img src={`/images/news/${article.image}`} alt={article.title} />
-                        <span>{article.title}</span>
-                    </Link>
-                </li>))}
-            </ul>
+            <NewsList news={DUMMY_NEWS} />
         </>
     );
 };
