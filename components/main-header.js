c533bb601c2a5c074f24252ba4ad19a24555df12
Angel <angel.pashov@proton.me>
2025-02-01 21:13:08 +0200

    added archive to navigation

--- components/main-header.js
+++ components/main-header.js
@@ -1,20 +1,42 @@
+'use client';
+
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const MainHeader = () => {
+    const path = usePathname();
+
     return (
-       <header id="main-header">
+        <header id="main-header">
             <div id="logo">
                 <Link href="/">NextNews</Link>
             </div>
             <nav>
                 <ul>
                     <li>
-                        <Link href="/news">News</Link>
+                        <Link
+                            href="/news"
+                            className={
+                                path.startsWith("/news") ? "active" : undefined
+                            }
+                        >
+                            News
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="/archive"
+                            className={
+                                path.startsWith("/archive") ? "active" : undefined
+                            }
+                        >
+                            Archive
+                        </Link>
                     </li>
                 </ul>
             </nav>
-       </header>
+        </header>
     );
 };
 
