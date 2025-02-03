9aae2769c0f93735451ccc828a8ea8da7838e7bd
Angel <angel.pashov@proton.me>
2025-02-03 13:45:54 +0200

    Added nav link component

--- components/main-header.js
+++ components/main-header.js
@@ -1,12 +1,8 @@
-'use client';
-
 import React from "react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import NavLink from "./nav-link";
 
 const MainHeader = () => {
-    const path = usePathname();
-
     return (
         <header id="main-header">
             <div id="logo">
@@ -15,24 +11,10 @@ const MainHeader = () => {
             <nav>
                 <ul>
                     <li>
-                        <Link
-                            href="/news"
-                            className={
-                                path.startsWith("/news") ? "active" : undefined
-                            }
-                        >
-                            News
-                        </Link>
+                        <NavLink href="/news">News</NavLink>
                     </li>
                     <li>
-                        <Link
-                            href="/archive"
-                            className={
-                                path.startsWith("/archive") ? "active" : undefined
-                            }
-                        >
-                            Archive
-                        </Link>
+                        <NavLink href="/archive">Archive</NavLink>
                     </li>
                 </ul>
             </nav>
