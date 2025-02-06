cce10fa68996ae3a9dba796cf4eeb148fc886e02
Angel <angel.pashov@proton.me>
2025-02-06 18:27:13 +0200

    Added reactquery and a fetch function

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,13 +1,28 @@
-import React from "react";
+"use client";
 
-import { DUMMY_NEWS } from "@/dummy-news";
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
 import NewsList from "@/components/news-list";
+import fetchNews from "@/lib/newsApi";
 
 const NewsPage = () => {
+    const { isLoading, isError, data, error } = useQuery({
+        queryKey: ["news"],
+        queryFn: fetchNews,
+    });
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (isError) {
+        return <div>Error: {error.message}</div>;
+    }
+
     return (
         <>
             <h1>News Page</h1>
-            <NewsList news={DUMMY_NEWS} />
+            <NewsList news={data} />
         </>
     );
 };
--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,15 @@
-import './globals.css';
- 
+import "./globals.css";
+import ReactQueryProvider from "./providers/ReactQueryProvider";
+
 export const metadata = {
-  title: 'Next.js Page Routing & Rendering',
-  description: 'Learn how to route to different pages.',
+    title: "Next.js Page Routing & Rendering",
+    description: "Learn how to route to different pages.",
 };
- 
+
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body><ReactQueryProvider>{children}</ReactQueryProvider></body>
+        </html>
+    );
 }
